276b66cd5b5ab686dcbc98169533f4293e00138e
Artem Belyakov <pochtaforfkn@gmail.com>
2021-02-27 11:02:37 +0300

    feat: add video stream handle

--- src/routers/video.py
+++ src/routers/video.py
@@ -1,9 +1,15 @@
 import uuid
 
-from fastapi import APIRouter, UploadFile, File
-from pydantic import BaseModel
+from fastapi import (
+    APIRouter,
+    UploadFile,
+    File,
+    HTTPException,
+)
+from fastapi.responses import StreamingResponse
 
 from src.config import VIDEO_DIR
+from src.utils import video_frames_generator
 
 
 router = APIRouter(
@@ -12,6 +18,7 @@ router = APIRouter(
 
 
 @router.post("/upload")
+# TODO(belyakov): get more metadata and save it to db
 async def video_upload(name: str, description: str, file: UploadFile = File(...)):
     # TODO(belyakov): check filename for . and /
 
@@ -19,6 +26,7 @@ async def video_upload(name: str, description: str, file: UploadFile = File(...)
     # TODO(belyakov): to something with file extension
     video_path = VIDEO_DIR + str(video_uuid) + ".mp4"
 
+    # save file to local filesystem
     with open(video_path, "wb+") as f:
         f.write(await file.read())
 
@@ -29,3 +37,20 @@ async def video_upload(name: str, description: str, file: UploadFile = File(...)
         "description": description,
     }
 
+
+@router.get("/stream")
+async def video_stream(name: str):
+    # TODO(belyakov): accept more metadata and get uuid from db
+    filename = VIDEO_DIR + name + ".mp4"
+
+    # check if file exists
+    try:
+        f = open(filename)
+    except IOError:
+        # response with not found error
+        raise HTTPException(status_code=404, detail="No video with such name")
+
+    return StreamingResponse(
+        content=video_frames_generator(filename),
+        media_type="multipart/x-mixed-replace; boundary=frame",
+    )
